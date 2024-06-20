d91e642e184005e53388cca92a0721d7dfe7620f
FarhanAhd <trixiephany@gmail.com>
2024-06-20 15:44:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/malayalamsupportedlogin/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.malayalamsupportedlogin
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun myClickHandler(view: View) {
+        Log.i("MainActivity","button clicked")
+        var webIntent: Intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
+        startActivity(webIntent)
+    }
+
+}
